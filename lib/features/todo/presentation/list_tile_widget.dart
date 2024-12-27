0f92babe23de066ed49404f2038c2f1d92c6617f
jeankpoti <kpotistar@gmail.com>
2024-12-27 02:14:36 -0600

    Add multi select delete

--- lib/features/todo/presentation/list_tile_widget.dart
+++ lib/features/todo/presentation/list_tile_widget.dart
@@ -10,12 +10,35 @@ import '../../../common_widget/text_form_field_widget.dart';
 import '../data/models/isar_todo.dart';
 import '../domain/models/todo.dart';
 
+import 'package:flutter/material.dart';
+import 'package:isar/isar.dart';
+
+import '../../../common_widget/icon_widget.dart';
+import '../../../common_widget/text_widget.dart';
+import '../../../common_widget/button_widget.dart';
+import '../../../common_widget/text_form_field_widget.dart';
+import '../data/models/isar_todo.dart';
+import '../domain/models/todo.dart';
+import 'todo_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 class ListTileWidget extends StatefulWidget {
   final String? title, desc;
   final Todo todoList;
   final bool isTrailingVisible;
   bool isChecked = false;
   bool isCompleted = false;
+
+  // ------------------- ADDED FIELDS FOR MULTI-SELECTION ---------------------
+  /// Whether the app is currently in multi-selection mode.
+  final bool isSelectionMode;
+
+  /// Whether this specific todo is selected in multi-selection mode.
+  final bool isSelected;
+
+  /// Callback when user toggles selection of this todo in multi-selection mode.
+  final ValueChanged<bool?>? onSelected;
+  // --------------------------------------------------------------------------
 
   ListTileWidget({
     super.key,
@@ -25,6 +48,12 @@ class ListTileWidget extends StatefulWidget {
     this.isTrailingVisible = true,
     this.isChecked = false,
     this.isCompleted = false,
+
+    // ------------------- DEFAULT VALUES FOR NEW FIELDS ----------------------
+    this.isSelectionMode = false,
+    this.isSelected = false,
+    this.onSelected,
+    // ------------------------------------------------------------------------
   });
 
   @override
@@ -55,18 +84,22 @@ class _ListTileWidgetState extends State<ListTileWidget>
     _fadeOutAnimation = Tween<double>(
       begin: 1.0,
       end: 0.0,
-    ).animate(CurvedAnimation(
-      parent: _animationController,
-      curve: Curves.easeOut,
-    ));
+    ).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeOut,
+      ),
+    );
 
     _slideAnimation = Tween<Offset>(
       begin: Offset.zero,
       end: const Offset(1.0, 0.0),
-    ).animate(CurvedAnimation(
-      parent: _animationController,
-      curve: Curves.easeOut,
-    ));
+    ).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeOut,
+      ),
+    );
   }
 
   @override
@@ -102,15 +135,11 @@ class _ListTileWidgetState extends State<ListTileWidget>
               text: 'Are you sure you want to delete this todo?'),
           actions: <Widget>[
             TextButton(
-              child: const TextWidget(
-                text: 'Cancel',
-              ),
+              child: const TextWidget(text: 'Cancel'),
               onPressed: () => Navigator.pop(context),
             ),
             TextButton(
-              child: const TextWidget(
-                text: 'Delete',
-              ),
+              child: const TextWidget(text: 'Delete'),
               onPressed: () {
                 todoCubit.deleteTodo(todo);
                 Navigator.pop(context);
@@ -126,7 +155,6 @@ class _ListTileWidgetState extends State<ListTileWidget>
     switch (value) {
       case 'Edit':
         _showBottomSheet(context, todo);
-
         break;
       case 'Delete':
         showMyDialog(todo);
@@ -144,7 +172,6 @@ class _ListTileWidgetState extends State<ListTileWidget>
         ..isCompleted = todo.isCompleted;
 
       final todoCubit = context.read<TodoCubit>();
-
       todoCubit.updateTodo(todoToUpdate);
 
       _titleController.clear();
@@ -167,7 +194,6 @@ class _ListTileWidgetState extends State<ListTileWidget>
           ),
           decoration: BoxDecoration(
             color: Theme.of(context).colorScheme.surface,
-            // border: Border.all(),
             borderRadius: BorderRadius.circular(10),
             boxShadow: const [
               BoxShadow(
@@ -177,15 +203,30 @@ class _ListTileWidgetState extends State<ListTileWidget>
               ),
             ],
           ),
+          // Keep your existing "isCompleted" logic
           child: widget.todoList.isCompleted
               ? const SizedBox.shrink()
               : ListTile(
                   title: TextWidget(text: widget.todoList.title),
-                  subtitle: TextWidget(text: widget.todoList.description ?? ''),
-                  leading: Checkbox(
-                      value: widget.todoList.isCompleted,
-                      onChanged: (value) =>
-                          todoCubit.toggleTodoStatus(widget.todoList)),
+                  subtitle: TextWidget(
+                    text: widget.todoList.description ?? '',
+                  ),
+
+                  // ----------------- MULTI-SELECTION OR COMPLETION CHECKBOX -----------------
+                  leading: widget.isSelectionMode
+                      ? Checkbox(
+                          value: widget.isSelected,
+                          // If onSelected is null, do nothing to avoid errors
+                          onChanged: widget.onSelected ?? (_) {},
+                        )
+                      : Checkbox(
+                          value: widget.todoList.isCompleted,
+                          onChanged: (value) =>
+                              todoCubit.toggleTodoStatus(widget.todoList),
+                        ),
+                  // ---------------------------------------------------------------------------
+
+                  // Keep your existing trailing with pop-up menu, edit, delete
                   trailing: widget.isCompleted
                       ? const SizedBox.shrink()
                       : SizedBox(
@@ -260,7 +301,7 @@ class _ListTileWidgetState extends State<ListTileWidget>
                   ButtonWidget(
                     onPressed: () => {
                       validateAndUpdate(context, todo),
-                      Navigator.pop(context)
+                      Navigator.pop(context),
                     },
                     text: 'Update'.toUpperCase(),
                   ),
@@ -273,3 +314,267 @@ class _ListTileWidgetState extends State<ListTileWidget>
     );
   }
 }
+
+// class ListTileWidget extends StatefulWidget {
+//   final String? title, desc;
+//   final Todo todoList;
+//   final bool isTrailingVisible;
+//   bool isChecked = false;
+//   bool isCompleted = false;
+
+//   ListTileWidget({
+//     super.key,
+//     this.title,
+//     this.desc,
+//     required this.todoList,
+//     this.isTrailingVisible = true,
+//     this.isChecked = false,
+//     this.isCompleted = false,
+//   });
+
+//   @override
+//   State<ListTileWidget> createState() => _ListTileWidgetState();
+// }
+
+// class _ListTileWidgetState extends State<ListTileWidget>
+//     with SingleTickerProviderStateMixin {
+//   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+//   final _titleController = TextEditingController();
+//   final _descriptionController = TextEditingController();
+
+//   late AnimationController _animationController;
+//   late Animation<double> _fadeOutAnimation;
+//   late Animation<Offset> _slideAnimation;
+
+//   final isar = Isar.getInstance();
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _animationController = AnimationController(
+//       duration: const Duration(milliseconds: 500),
+//       vsync: this,
+//     );
+
+//     _fadeOutAnimation = Tween<double>(
+//       begin: 1.0,
+//       end: 0.0,
+//     ).animate(CurvedAnimation(
+//       parent: _animationController,
+//       curve: Curves.easeOut,
+//     ));
+
+//     _slideAnimation = Tween<Offset>(
+//       begin: Offset.zero,
+//       end: const Offset(1.0, 0.0),
+//     ).animate(CurvedAnimation(
+//       parent: _animationController,
+//       curve: Curves.easeOut,
+//     ));
+//   }
+
+//   @override
+//   void dispose() {
+//     _animationController.dispose();
+//     super.dispose();
+//   }
+
+//   List<Map<String, dynamic>> get choices => [
+//         {
+//           'title': 'Edit',
+//           'icon': const IconWidget(
+//             icon: Icons.edit,
+//           ),
+//         },
+//         {
+//           'title': 'Delete',
+//           'icon': const IconWidget(
+//             icon: Icons.delete,
+//           ),
+//         },
+//       ];
+
+//   Future<void> showMyDialog(Todo todo) async {
+//     final todoCubit = context.read<TodoCubit>();
+//     return showDialog<void>(
+//       context: context,
+//       barrierDismissible: false,
+//       builder: (BuildContext context) {
+//         return AlertDialog(
+//           backgroundColor: Theme.of(context).colorScheme.surface,
+//           title: const TextWidget(
+//               text: 'Are you sure you want to delete this todo?'),
+//           actions: <Widget>[
+//             TextButton(
+//               child: const TextWidget(
+//                 text: 'Cancel',
+//               ),
+//               onPressed: () => Navigator.pop(context),
+//             ),
+//             TextButton(
+//               child: const TextWidget(
+//                 text: 'Delete',
+//               ),
+//               onPressed: () {
+//                 todoCubit.deleteTodo(todo);
+//                 Navigator.pop(context);
+//               },
+//             ),
+//           ],
+//         );
+//       },
+//     );
+//   }
+
+//   void handleClick(String value, Todo todo) {
+//     switch (value) {
+//       case 'Edit':
+//         _showBottomSheet(context, todo);
+
+//         break;
+//       case 'Delete':
+//         showMyDialog(todo);
+//         break;
+//     }
+//   }
+
+//   void validateAndUpdate(buildContext, todo) async {
+//     final FormState form = _formKey.currentState!;
+//     if (form.validate()) {
+//       final todoToUpdate = TodoIsar()
+//         ..id = todo.id
+//         ..title = _titleController.text
+//         ..description = _descriptionController.text
+//         ..isCompleted = todo.isCompleted;
+
+//       final todoCubit = context.read<TodoCubit>();
+
+//       todoCubit.updateTodo(todoToUpdate);
+
+//       _titleController.clear();
+//       _descriptionController.clear();
+//     } else {}
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final todoCubit = context.read<TodoCubit>();
+
+//     return SlideTransition(
+//       position: _slideAnimation,
+//       child: FadeTransition(
+//         opacity: _fadeOutAnimation,
+//         child: Container(
+//           margin: const EdgeInsets.symmetric(
+//             horizontal: 12.0,
+//             vertical: 4.0,
+//           ),
+//           decoration: BoxDecoration(
+//             color: Theme.of(context).colorScheme.surface,
+//             // border: Border.all(),
+//             borderRadius: BorderRadius.circular(10),
+//             boxShadow: const [
+//               BoxShadow(
+//                 color: Colors.black12,
+//                 offset: Offset(0, 2),
+//                 blurRadius: 6.0,
+//               ),
+//             ],
+//           ),
+//           child: widget.todoList.isCompleted
+//               ? const SizedBox.shrink()
+//               : ListTile(
+//                   title: TextWidget(text: widget.todoList.title),
+//                   subtitle: TextWidget(text: widget.todoList.description ?? ''),
+//                   leading: Checkbox(
+//                       value: widget.todoList.isCompleted,
+//                       onChanged: (value) =>
+//                           todoCubit.toggleTodoStatus(widget.todoList)),
+//                   trailing: widget.isCompleted
+//                       ? const SizedBox.shrink()
+//                       : SizedBox(
+//                           width: 50,
+//                           child: Row(
+//                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                             children: [
+//                               widget.isTrailingVisible
+//                                   ? PopupMenuButton<String>(
+//                                       icon: const IconWidget(
+//                                           icon: Icons.more_vert),
+//                                       onSelected: (String value) {
+//                                         handleClick(value, widget.todoList);
+//                                       },
+//                                       itemBuilder: (BuildContext context) {
+//                                         return choices.map((choice) {
+//                                           return PopupMenuItem<String>(
+//                                             value: choice['title'].toString(),
+//                                             child: ListTile(
+//                                               leading: choice['icon'],
+//                                               title: Text(
+//                                                   choice['title'].toString()),
+//                                             ),
+//                                           );
+//                                         }).toList();
+//                                       },
+//                                     )
+//                                   : const SizedBox.shrink(),
+//                             ],
+//                           ),
+//                         ),
+//                 ),
+//         ),
+//       ),
+//     );
+//   }
+
+//   void _showBottomSheet(BuildContext context, Todo todo) {
+//     _titleController.text = todo.title;
+//     _descriptionController.text = todo.description ?? '';
+//     showModalBottomSheet(
+//       context: context,
+//       builder: (BuildContext context) {
+//         return Container(
+//           padding: const EdgeInsets.all(16.0),
+//           height: 400,
+//           width: double.infinity,
+//           child: Padding(
+//             padding: const EdgeInsets.only(
+//               left: 16,
+//               top: 50,
+//               right: 16,
+//               bottom: 50,
+//             ),
+//             child: Form(
+//               key: _formKey,
+//               child: Column(
+//                 spacing: 20,
+//                 children: [
+//                   TextFormFieldWidget(
+//                     controller: _titleController,
+//                     labelText: 'Enter your todo\'s title',
+//                     validator: (value) =>
+//                         value!.isEmpty ? 'Please provide a title' : null,
+//                   ),
+//                   TextFormFieldWidget(
+//                     controller: _descriptionController,
+//                     labelText: 'Enter your todo\'s description',
+//                     validator: (value) => value!.isEmpty ? null : '',
+//                   ),
+//                   const SizedBox(height: 25),
+//                   ButtonWidget(
+//                     onPressed: () => {
+//                       validateAndUpdate(context, todo),
+//                       Navigator.pop(context)
+//                     },
+//                     text: 'Update'.toUpperCase(),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//         );
+//       },
+//     );
+//   }
+// }
--- lib/features/todo/presentation/todo_view.dart
+++ lib/features/todo/presentation/todo_view.dart
@@ -10,9 +10,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../common_widget/app_bar_widget.dart';
 import '../../../common_widget/button_widget.dart';
 import '../../../common_widget/text_form_field_widget.dart';
-import '../../../common_widget/text_widget.dart';
 import '../domain/models/todo.dart';
-import 'add_todo_page.dart';
 import 'list_tile_widget.dart';
 import 'todo_cubit.dart';
 
@@ -24,6 +22,9 @@ class TodoView extends StatefulWidget {
 }
 
 class _TodoViewState extends State<TodoView> {
+  bool isSelectionMode = false;
+  final Set<Todo> selectedTodos = {};
+
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   final _titleController = TextEditingController();
@@ -40,43 +41,34 @@ class _TodoViewState extends State<TodoView> {
     } else {}
   }
 
-  // Show dialog box to add new todo
-  // void _showAddTodoBox(BuildContext context) {
-  //   final todoCubit = context.read<TodoCubit>();
-
-  //   final textController = TextEditingController();
-
-  //   showDialog(
-  //       context: context,
-  //       builder: (context) => AlertDialog(
-  //             title: const Text('Add Todo'),
-  //             content: TextField(controller: textController),
-  //             actions: [
-  //               TextButton(
-  //                 onPressed: () => Navigator.pop(context),
-  //                 child: const Text('Cancel'),
-  //               ),
-  //               TextButton(
-  //                 onPressed: () {
-  //                   todoCubit.addTodo(textController.text);
-  //                   Navigator.pop(context);
-  //                 },
-  //                 child: const Text('Add'),
-  //               ),
-  //             ],
-  //           ));
-  // }
-
   @override
   Widget build(BuildContext context) {
-    // final todoCubit = context.read<TodoCubit>();
-    // final themeCubit = context.read<ThemeCubit>();
-
+    // Example usage of your existing code to build the list
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      appBar: const AppBarWidget(
-        title: 'Todo List',
-        isAction: false,
+      appBar: AppBar(
+        title: Text(
+          'Todos',
+          style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+        ),
+        actions: [
+          if (isSelectionMode) ...[
+            IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: selectedTodos.isEmpty ? null : _deleteSelectedTodos,
+            ),
+            IconButton(
+              icon: const Icon(Icons.close),
+              onPressed: () {
+                setState(() {
+                  isSelectionMode = false;
+                  selectedTodos.clear();
+                });
+              },
+            )
+          ]
+        ],
       ),
       body: BlocBuilder<TodoCubit, List<Todo>>(
         builder: (context, todos) {
@@ -84,10 +76,31 @@ class _TodoViewState extends State<TodoView> {
             itemCount: todos.length,
             itemBuilder: (context, index) {
               final todo = todos[index];
-              return ListTileWidget(
-                todoList: todo,
-                title: todo.title,
-                desc: todo.description,
+
+              return GestureDetector(
+                onLongPress: () {
+                  // Enter multi-select mode
+                  setState(() {
+                    isSelectionMode = true;
+                  });
+                },
+                child: ListTileWidget(
+                  todoList: todo,
+                  // Keep your existing parameters
+                  isTrailingVisible: true,
+                  // New multi-select parameters:
+                  isSelectionMode: isSelectionMode,
+                  isSelected: selectedTodos.contains(todo),
+                  onSelected: (bool? selected) {
+                    setState(() {
+                      if (selected == true) {
+                        selectedTodos.add(todo);
+                      } else {
+                        selectedTodos.remove(todo);
+                      }
+                    });
+                  },
+                ),
               );
             },
           );
@@ -154,4 +167,155 @@ class _TodoViewState extends State<TodoView> {
       },
     );
   }
+
+  void _deleteSelectedTodos() {
+    final todoCubit = context.read<TodoCubit>();
+    for (final todo in selectedTodos) {
+      todoCubit.deleteTodo(todo);
+    }
+    setState(() {
+      selectedTodos.clear();
+      isSelectionMode = false;
+    });
+  }
 }
+
+// class TodoView extends StatefulWidget {
+//   const TodoView({super.key});
+
+//   @override
+//   State<TodoView> createState() => _TodoViewState();
+// }
+
+// class _TodoViewState extends State<TodoView> {
+  // final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  // final _titleController = TextEditingController();
+  // final _descriptionController = TextEditingController();
+
+  // void validateAndSave(buildContext) {
+  //   final FormState form = _formKey.currentState!;
+  //   if (form.validate()) {
+  //     final todoCubit = context.read<TodoCubit>();
+  //     todoCubit.addTodo(_titleController.text, _descriptionController.text);
+
+  //     _titleController.clear();
+  //     _descriptionController.clear();
+  //   } else {}
+  // }
+
+//   // Show dialog box to add new todo
+//   // void _showAddTodoBox(BuildContext context) {
+//   //   final todoCubit = context.read<TodoCubit>();
+
+//   //   final textController = TextEditingController();
+
+//   //   showDialog(
+//   //       context: context,
+//   //       builder: (context) => AlertDialog(
+//   //             title: const Text('Add Todo'),
+//   //             content: TextField(controller: textController),
+//   //             actions: [
+//   //               TextButton(
+//   //                 onPressed: () => Navigator.pop(context),
+//   //                 child: const Text('Cancel'),
+//   //               ),
+//   //               TextButton(
+//   //                 onPressed: () {
+//   //                   todoCubit.addTodo(textController.text);
+//   //                   Navigator.pop(context);
+//   //                 },
+//   //                 child: const Text('Add'),
+//   //               ),
+//   //             ],
+//   //           ));
+//   // }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // final todoCubit = context.read<TodoCubit>();
+//     // final themeCubit = context.read<ThemeCubit>();
+
+//     return Scaffold(
+//       backgroundColor: Theme.of(context).colorScheme.surface,
+//       appBar: const AppBarWidget(
+//         title: 'Todo List',
+//         isAction: false,
+//       ),
+//       body: BlocBuilder<TodoCubit, List<Todo>>(
+//         builder: (context, todos) {
+//           return ListView.builder(
+//             itemCount: todos.length,
+//             itemBuilder: (context, index) {
+//               final todo = todos[index];
+//               return ListTileWidget(
+//                 todoList: todo,
+//                 title: todo.title,
+//                 desc: todo.description,
+//               );
+//             },
+//           );
+//         },
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () => _showBottomSheet(context),
+//         // Navigator.push(
+//         //   context,
+//         //   MaterialPageRoute(
+//         //     builder: (context) => const AddTodoPage(),
+//         //   ),
+//         // ),
+//         // _showAddTodoBox(context),
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+
+  // void _showBottomSheet(BuildContext context) {
+  //   showModalBottomSheet(
+  //     context: context,
+  //     builder: (BuildContext context) {
+  //       return Container(
+  //         padding: const EdgeInsets.all(16.0),
+  //         height: 400,
+  //         width: double.infinity,
+  //         child: Padding(
+  //           padding: const EdgeInsets.only(
+  //             left: 16,
+  //             top: 50,
+  //             right: 16,
+  //             bottom: 50,
+  //           ),
+  //           child: Form(
+  //             key: _formKey,
+  //             child: Column(
+  //               spacing: 20,
+  //               children: [
+  //                 TextFormFieldWidget(
+  //                   controller: _titleController,
+  //                   labelText: 'Enter your todo\'s title',
+  //                   validator: (value) =>
+  //                       value!.isEmpty ? 'Please provide a title' : null,
+  //                 ),
+  //                 TextFormFieldWidget(
+  //                   controller: _descriptionController,
+  //                   labelText: 'Enter your todo\'s description',
+  //                   validator: (value) => value!.isEmpty ? null : '',
+  //                 ),
+  //                 const SizedBox(height: 25),
+  //                 ButtonWidget(
+  //                   onPressed: () => {
+  //                     validateAndSave(context),
+  //                     Navigator.pop(context),
+  //                   },
+  //                   text: 'Save'.toUpperCase(),
+  //                 ),
+  //               ],
+  //             ),
+  //           ),
+  //         ),
+  //       );
+  //     },
+  //   );
+  // }
+// }
